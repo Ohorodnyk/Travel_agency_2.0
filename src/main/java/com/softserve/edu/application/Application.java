096b68725894f42234e73f1ba2e77f841d5edfd5
Roman Karpliak <karpliakroman@gmail.com>
2017-01-21 16:06:51 +0200

    aplication

--- src/main/java/com/softserve/edu/application/Application.java
+++ src/main/java/com/softserve/edu/application/Application.java
@@ -1,4 +1,30 @@
 package com.softserve.edu.application;
 
+import java.util.List;
+
+
+import org.hibernate.Session;
+import com.softserve.edu.dao.HibernateUtil;
+import com.softserve.edu.entity.Country;
+
 public class Application {
+    public static void main(String[] args)
+    {
+    Session session = HibernateUtil.getSessionFactory().openSession();
+    try {
+        
+        
+        org.hibernate.Transaction transaction = session.beginTransaction();
+        
+        transaction.commit();
+        
+        
+    } catch (Exception e) {
+        e.printStackTrace();
+    }finally{
+        session.close();
+        HibernateUtil.getSessionFactory().close();
+        
+    }
+}
 }
